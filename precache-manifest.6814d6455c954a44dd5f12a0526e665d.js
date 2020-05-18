80956a2753a30b7ec3612820a7cd434ecc87d2e3
thepositivepiet <59098232+thepositivepiet@users.noreply.github.com>
2020-05-17 20:47:40 -0500

    Updates

---
+++ precache-manifest.6814d6455c954a44dd5f12a0526e665d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "463e9a6307a5a4226801d467c177d655",
+    "url": "/magpiet/facerecognitionapp/index.html"
+  },
+  {
+    "revision": "b646f2c2232cf96cd077",
+    "url": "/magpiet/facerecognitionapp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "609b6e06add63f8d36c1",
+    "url": "/magpiet/facerecognitionapp/static/css/main.cb5b8190.chunk.css"
+  },
+  {
+    "revision": "b646f2c2232cf96cd077",
+    "url": "/magpiet/facerecognitionapp/static/js/2.33fef3fb.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/magpiet/facerecognitionapp/static/js/2.33fef3fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "609b6e06add63f8d36c1",
+    "url": "/magpiet/facerecognitionapp/static/js/main.43af61d2.chunk.js"
+  },
+  {
+    "revision": "b2162b5e7beb26b22104",
+    "url": "/magpiet/facerecognitionapp/static/js/runtime-main.937f97db.js"
+  }
+]);
