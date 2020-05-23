3b6c31840f7975af9145c5c4595aeb910470c1da
thepositivepiet <59098232+thepositivepiet@users.noreply.github.com>
2020-05-22 23:56:58 -0500

    deploying front end

--- src/App.js
+++ src/App.js
@@ -5,14 +5,9 @@ import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
 import SignIn from './Components/SignIn/SignIn';
 import Register from './Components/Register/Register';
-
-const app = new Clarifai.App({
-  apiKey: 'ba457b9baafd46189fc17bf95a702406'
-});
 
 const particleOptions = {
   particles: {
@@ -26,16 +21,35 @@ const particleOptions = {
   }
 }
 
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
+
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false
-    }
+    this.state = initialState;
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
   }
 
   calculateFaceLocation = (data) => {
@@ -60,17 +74,39 @@ class App extends Component {
   }
 
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input})
-    app.models
-    .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    this.setState({imageUrl: this.state.input});
+    fetch('http://localhost:3001/imageurl', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+    .then(response => response.json())
+    .then(response => {
+      if (response) {
+        fetch('http://localhost:3001/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+           id: this.state.user.id
+          })
+        })
+        .then(response => response.json())
+        .then(count => {
+          this.setState(Object.assign(this.state.user, {entries: count}))
+        })
+        .catch(console.log);
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response));
+    })
     .catch(error => console.log(error))
   }
 
   onRouteChange = (route) => {
     this.setState({route: route});
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
@@ -87,7 +123,7 @@ class App extends Component {
         {route === 'home' ?
           <div>
             <Logo />
-            <Rank />
+            <Rank name={this.state.user.name} entries={this.state.user.entries}/>
             <ImageLinkForm 
             onInputChange={this.onInputChange} 
             onButtonSubmit={this.onButtonSubmit}
@@ -96,8 +132,8 @@ class App extends Component {
           </div>
           : (
             route === 'signin' ?
-            <SignIn onRouteChange={this.onRouteChange} />
-            : <Register onRouteChange={this.onRouteChange} />
+            <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
           )
         }
       </div>
--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -6,9 +6,9 @@ import brain from './brain.png';
 const Logo = () => {
 	return (
 		<div className='ma4 mt0'>
-			<Tilt className="Tilt br2 shadow-2" options={{ max : 30 }} style={{ height: 150, width: 150 }} >
+			<Tilt className="center Tilt br2 shadow-2" options={{ max : 30 }} style={{ height: 150, width: 150 }} >
 				<div className="Tilt-inner pa3">
-					<img style={{paddingTop: '20px'}} alt='logo' src={brain}/>
+					<img style={{paddingTop: '25px'}} alt='logo' src={brain}/>
 				</div>
 			</Tilt>
 		</div>
